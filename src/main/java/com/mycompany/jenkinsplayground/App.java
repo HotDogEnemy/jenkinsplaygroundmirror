9cae59297a29c9b571428b2c7e6939f2f6cf0467
Shivodit Gill <shivodit.gill@sap.com>
2024-10-14 17:12:53 +0530

    Mirroring code for revision b3f109c8326f020629cfa1959dce9dba991aab48
    from https://github.com/HotDogEnemy/jenkinsplayground.git

---
+++ src/main/java/com/mycompany/jenkinsplayground/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.jenkinsplayground;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = "MyName";
+        System.out.println( "Hello World!" + name );
+    }
+}
